42e1de955348f69473326bb90d18fe8bb73b7fca
vlad-tudor <59935721+vlad-tudor@users.noreply.github.com>
2025-12-04 00:35:59 +0000

    day2 part2

--- day1/part2.ts
+++ day1/part2.ts
@@ -6,22 +6,21 @@ export default function part2() {
 
   // NOTE: day2 input is the same as day1
   // const lines = readLines("./day1/input-p1.txt");
-  const lines = readLines("./day1/input-test.txt");
+  const lines = readLines("./day1/input-p1.txt");
 
   // counts amounts of time pointer is at 0
   let clicks = 0;
 
-  for (const line of lines.slice(0, 4)) {
+  for (const line of lines) {
     const rotation = parseRotation(line);
-    const newPointer = pointer + rotation;
 
-    // checks the difference in 'hundreds' which has occurred,
-    // which signifies how many times the needle has gone past 0
-    const zeroClicks = Math.floor(newPointer / 100) - Math.floor(pointer / 100);
-
-    clicks += Math.abs(zeroClicks);
-
-    pointer = newPointer;
+    // I feel shame
+    for (let index = 0; index < Math.abs(rotation); index++) {
+      pointer += rotation > 0 ? 1 : -1;
+      if (pointer % 100 === 0) {
+        clicks++;
+      }
+    }
   }
 
   return { clicks };
